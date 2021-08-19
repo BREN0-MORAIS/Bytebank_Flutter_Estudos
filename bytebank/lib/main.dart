0e87e500cbbdead13fb6b9005483b9d7570d49f0
Breno <desenvolvimento1@wcogeo.com.br>
2021-08-19 12:34:46 -0400

    atualização, 19-08-2021

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -1,38 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp
-(
-  Column(
-    children: [
-      Text(
-        'Bem Vindo AO Bytebank',
-        textDirection: TextDirection.ltr,
-      ),
-      Text(
-        'Bem Vindo AO Bytebank',
-        textDirection: TextDirection.ltr,
-      ),
-      Column(
-        children: [
-          Text(
-            'Bem Vindo AO Bytebank',
-            textDirection: TextDirection.ltr,
-          ),
-          Text(
-            'Bem Vindo AO Bytebank',
-            textDirection: TextDirection.ltr,
-          ),
-        ],
+void main() => runApp(MaterialApp(
 
-      ),
+  home: Scaffold(
+    appBar:AppBar(title: Text('Transferencias'),),
+    floatingActionButton:FloatingActionButton(
+      child: Icon(Icons.add),
+      onPressed: null,
+    ),
+    body: Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on,
+          color:Colors.red ),
 
-    ],
+        title: Text('100.0'),
+        subtitle: Text('1000'),
+      ),
+      )
+    ),
+
+
 
   ),
-
-
-
-
 );
+
+
 
 

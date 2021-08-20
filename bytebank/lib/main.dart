46a35da8b7a0c31952754eeb4f5215c3384865fe
Breno <desenvolvimento1@wcogeo.com.br>
2021-08-20 12:39:09 -0400

    03-Refatorando- andamento

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -1,29 +1,47 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(
+      MaterialApp(
+          home: Scaffold(
+        appBar: AppBar(
+          title: Text('Transferencias'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: null,
+        ),
+        body: ListaTransferencia(),
+      )),
+    );
 
-  home: Scaffold(
-    appBar:AppBar(title: Text('Transferencias'),),
-    floatingActionButton:FloatingActionButton(
-      child: Icon(Icons.add),
-      onPressed: null,
-    ),
-    body: Card(
-      child: ListTile(
-        leading: Icon(Icons.monetization_on,
-          color:Colors.red ),
-
-        title: Text('100.0'),
-        subtitle: Text('1000'),
-      ),
-      )
-    ),
-
-
-
-  ),
-);
-
-
-
-
+class ListaTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      children: <Widget>[
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on, color: Colors.blue),
+            title: Text('100.0'),
+            subtitle: Text('1000'),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on, color: Colors.blue),
+            title: Text('200.0'),
+            subtitle: Text('2000'),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on, color: Colors.blue),
+            title: Text('400.0'),
+            subtitle: Text('4000'),
+          ),
+        ),
+      ],
+    );
+  }
+}
